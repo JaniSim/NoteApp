4b727fcab0b5329f20993901678e48ed4fd4b6c2
Jana Šimečková <jana.simeckova@thermofisher.com>
2023-12-17 13:51:37 +0100

    main page

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/note/presentation/notes/components/NotesScreen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/note/presentation/notes/components/NotesScreen.kt
@@ -1,5 +1,88 @@
 package com.plcoding.cleanarchitecturenoteapp.note.presentation.notes.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.plcoding.cleanarchitecturenoteapp.note.presentation.notes.NotesEvent
+import com.plcoding.cleanarchitecturenoteapp.note.presentation.notes.NotesVM
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.lazy.items
 
-class NotesScreen {
+@Composable
+fun NotesScreen(
+    navController: NavController,
+    viewModel: NotesVM = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+
+            }, backgroundColor = MaterialTheme.colors.primary) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
+            }
+        }, scaffoldState = scaffoldState){
+        Column( modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)){
+            Row (modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically){
+                Text(text = "Notes", style = MaterialTheme.typography.h4)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(modifier = Modifier.fillMaxSize()){
+                items(state.notes){ note ->
+                    NoteItem(
+                        note = note,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(NotesEvent.DeleteNote(note))
+                            scope.launch {
+                                val result = scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Note deleted",
+                                    actionLabel = "Undo"
+                                )
+                                if(result == SnackbarResult.ActionPerformed) {
+                                    viewModel.onEvent(NotesEvent.RestoreNote)
+                                }
+                            }
+                        })
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+
+        }
+    }
 }
